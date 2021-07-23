11a3698ed54c8185294f5036c95577f52dcd6847
surazregmy <suraz.regmy@gmail.com>
2021-07-23 07:33:22 +0545

    java 8 test

--- src/LambdaExpressionTest.java
+++ src/LambdaExpressionTest.java
@@ -10,7 +10,6 @@ public class LambdaExpressionTest {
         Student s4 = new Student("Shiva", "ktm", 20);
         Student s5 = new Student("Topa", "ktm", 19);
 
-
         List<Student> students = new ArrayList<>();
         students.add(s1);
         students.add(s2);
@@ -18,12 +17,28 @@ public class LambdaExpressionTest {
         students.add(s4);
         students.add(s5);
 
-        for (Student student : students) {
-            System.out.println(student.getName());
-        }
+//        for (Student student : students) {
+//            System.out.println(student.getName());
+//        }
 
+//        students.forEach(s -> System.out.println(s.getName()));
+        students.forEach(s -> System.out.println(s.getName()));
 
+        students.stream().filter(student -> student.getName().equalsIgnoreCase("Ram")).findAny().orElse(null);
 
+//        Student checkStudent = findStudentinList(students, "Ram");
+//        System.out.println(checkStudent.getLocation());
 
     }
+
+    public static Student findStudentinList(List<Student> students, String nametoFind) {
+        //check if the list of students contains student whose name is Ram
+        for (Student student : students) {
+            if (student.getName().equalsIgnoreCase(nametoFind)) {
+                return student;
+            }
+        }
+        return null;
+    }
 }
+
